e2f169eec1ae0c0c502e1dba3195403b721bf25b
Noury1991 <55098205+Noury1991@users.noreply.github.com>
2019-10-03 14:14:34 +0300

    Update my lab

--- ObjectLab.js
+++ ObjectLab.js
@@ -1,51 +1,49 @@
-// 1
+1// 1
 
-function Books(r)
-{
-const book1=[ {
-title:'the Hobbit',
-auther:'J.R.R',
-readed:false
-} ,
-
-{title:'Game of throuns',
-auther:'A.D.N',
-readed:true
-}  ];
-
-
-for (i=0; i<book1.length;i++)
-{
-   if (book1[i].readed=true){
-      console.log( "You already read "+""+book1[i].title+""+"by"+""+ Booksbook1[i].auther );
-   }
-   else {
-console.log( "You still need to read "+""+book1[i].title+""+"by"+""+ book1[i].auther );
-   }
-}
-  
-}
+const book=[ {
+    title:'the Hobbit',
+    auther:'J.R.R',
+    readed:false
+    } ,
+    
+    {title:'Game of throuns',
+    auther:'A.D.N',
+    readed:true
+    }  ];
+    
+    for (i=0; i<book.length;i++)
+    {
+        if (book[i].readed)
+        {
+        console.log( 'You already read "'+ book[i].title +'", by '+ book[i].auther );
+        
+        }
+    
+        else {
+            console.log( 'You still need to read "'+ book[i].title +'", by '+ book[i].auther );
+        }
+    }
 
 // 2
 const favoriteMovie = [
   {title: "The Godfather",
    duration: 50 ,
-    stars: "Marlon Brando, Al Pacino, James Caan"} ,
+    stars: ["Marlon Brando", "Al Pacino", "James Caan"]} ,
   {title: "The Dark Knight ",
    duration: 30,
-    stars: "Christian Bale, Heath Ledger, Aaron Eckhart"} ,
+    stars: ["Christian Bale", "Heath Ledger"," Aaron Eckhart"]} ,
   
     {title: "Inception",
    duration: 20,
-    stars: "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page "} 
+    stars: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"] } 
 ];
 
-function Fav()
-{
-  
+
 for (let i=0; i<favoriteMovie.length; i++)
   {
-console.log(favoriteMovie[i].join('-'));
-    i++;
+    
+console.log(favoriteMovie[i].title+ ' For ' + favoriteMovie[i].duration+' . Stars: '+(favoriteMovie[i].stars).join(' - '));
+    i++;  
   }
-}
+
+
